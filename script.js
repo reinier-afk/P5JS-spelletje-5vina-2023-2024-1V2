79a21d3f212ad506971f1996a1e479a5beb7842d
Julian-Nieuwenburg <127728022+Julian-Nieuwenburg@users.noreply.github.com>
2023-10-11 11:48:18 +0000

    Bom goes up and down

--- script.js
+++ script.js
@@ -1,64 +1,165 @@
-var xJager = 50;
-var yJager = 50;
-var xProoi = 800;
-var yProoi = 175;
+var raster = {
+  aantalRijen: 12,
+  aantalKolommen: 18,
+  celGrootte: null,
+  
+  berekenCelGrootte() {
+    this.celGrootte = canvas.width/this.aantalKolommen;
+  },
+  teken() {
+    push();
+    noFill();
+    stroke('grey');
+    for (rij=0;rij<this.aantalRijen;rij++) {
+      for (kolom=0;kolom<this.aantalKolommen;kolom++) {
+        rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
+      }
+    }
+    pop();
+  }
+}
+
+class bommen {
+  constructor() {
+    this.x = floor(random(9,18))*50;
+    this.y = floor(random(1,12))*50;
+        this.sprite = null;
+    this.stapGrootte = 1;
+  }
+
+  beweeg() {
+      if (this.y <= 25 || this.y >= 575) {
+      this.stapGrootte = -this.stapGrootte;
+    }
+    this.y += -this.stapGrootte
+
+  
+  }
+  
+  toon() {
+    image(this.sprite,this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}
+
+
+class Jos {
+  constructor() {
+    this.x = 0;
+    this.y = floor(random(1,12))*50;
+    this.animatie = [];
+    this.frameNummer =  3;
+    this.stapGrootte = null;
+    this.gehaald = false;
+  }
+  
+  beweeg() {
+    if (keyIsDown(65)) {
+      this.x -= this.stapGrootte;
+      this.frameNummer = 2;
+    }
+    if (keyIsDown(68)) {
+      this.x += this.stapGrootte;
+      this.frameNummer = 1;
+    }
+    if (keyIsDown(87)) {
+      this.y -= this.stapGrootte;
+      this.frameNummer = 4;
+    }
+    if (keyIsDown(83)) {
+      this.y += this.stapGrootte;
+      this.frameNummer = 5;
+    }
+    
+    this.x = constrain(this.x,0,canvas.width);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
+    
+    if (this.x == canvas.width) {
+      this.gehaald = true;
+    }
+  }
+  
+  wordtGeraakt(vijand) {
+    if (this.x == vijand.x && this.y == vijand.y) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  
+  toon() {
+    image(this.animatie[this.frameNummer],this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}
+
+class Vijand {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = null;
+    this.stapGrootte = null;
+  }
+
+  beweeg() {
+    this.x += floor(random(-1,2))*this.stapGrootte;
+    this.y += floor(random(-1,2))*this.stapGrootte;
+
+    this.x = constrain(this.x,0,canvas.width - raster.celGrootte);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
+  }
+  
+  toon() {
+    image(this.sprite,this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}
+
+function preload() {
+  brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
+}
 
 function setup() {
-  canvas = createCanvas(1000,400);
+  canvas = createCanvas(900,600);
   canvas.parent();
+  frameRate(5);
   textFont("Verdana");
-  textSize(140);
-  noStroke();
-  frameRate(50);
+  textSize(90);
+  raster.berekenCelGrootte();
+  
+  eve = new Jos();
+  eve.stapGrootte = 1*raster.celGrootte;
+  for (var b = 0;b < 6;b++) {
+    frameEve = loadImage("images/sprites/Eve100px/Eve_" + b + ".png");
+    eve.animatie.push(frameEve);
+  }
+  
+  alice = new Vijand(700,200);
+  alice.stapGrootte = 1*eve.stapGrootte;
+  alice.sprite = loadImage("images/sprites/Alice100px/Alice.png");
+
+  bom = new bommen();
+  bom.stapGrootte = 1*eve.stapGrootte;
+  bom.sprite = loadImage("images/sprites/bom.png");
+  
 }
 
 function draw() {
-  background('olive');
-  if (keyIsDown(LEFT_ARROW)) {
-    xJager -= 5;
+  background(brug);
+  raster.teken();
+  eve.beweeg();
+  alice.beweeg();
+  bom.beweeg();
+  eve.toon();
+  alice.toon();
+  bom.toon();
+  
+  if (eve.wordtGeraakt(alice)) {
+    noLoop();
   }
-  if (keyIsDown(RIGHT_ARROW)) {
-    xJager += 5;
+  
+  if (eve.gehaald) {
+    background('green');
+    fill('white');
+    text("Je hebt gewonnen!",30,300);
+    noLoop();
   }
-  if (keyIsDown(UP_ARROW)) {
-    yJager -= 5;
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    yJager += 5;
-  }
-
-  if (keyIsDown(65)) {
-    xProoi -= 5;
-  }
-  if (keyIsDown(68)) {
-    xProoi += 5;
-  }
-  if (keyIsDown(87)) {
-    yProoi -= 5;
-  }
-  if (keyIsDown(83)) {
-    yProoi += 5;
-  }
-
-  xJager = constrain(xJager,0,width - 100);
-  yJager = constrain(yJager,0,height - 100);
-  xProoi = constrain(xProoi,0,width - 75);
-  yProoi = constrain(yProoi,0,height - 50);
-
-  if (xJager >= 700 && xJager <= 875 && yJager >= 75 && yJager <= 225) {
-    fill('chartreuse');
-  }
-  else {
-    fill('darkkhaki');
-  }
-  rect(xProoi,yProoi,75,50);
-  fill('moccasin');
-  rect(xJager,yJager,100,100);   
-}
-
-function eindScherm() {
-  background('white');
-  fill('black');
-  text("GEVANGEN!",75,250);
-  noLoop();
 }
